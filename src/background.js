e3c4f9a9e39c0bbf670d72b9a41cda02c29adad6
devwithsmile <devsaini7972@gmail.com>
2025-05-10 16:36:51 +0530

    new setup with backend

--- src/background.js
+++
@@ -1,2 +0,0 @@
-// Background script for the extension
-console.log('Background script loaded');
--- src/popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const runButton = document.getElementById('run');
-  
-  runButton.addEventListener('click', function() {
-    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {action: "takeEmail"});
-    });
-  });
-});
